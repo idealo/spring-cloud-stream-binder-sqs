cf57f010063c90f3d17894e7973c7bce07cdb2c2
heiko.rothe <heiko.rothe@idealo.de>
2021-04-29 18:11:36 +0200

    Add test for verifying consumer

--- src/test/java/de/idealo/spring/stream/binder/sqs/SqsBinderIT.java
+++ src/test/java/de/idealo/spring/stream/binder/sqs/SqsBinderIT.java
@@ -3,6 +3,7 @@ package de.idealo.spring.stream.binder.sqs;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
+import java.time.Duration;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -22,6 +23,9 @@ import org.testcontainers.utility.DockerImageName;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
+import reactor.core.publisher.Sinks;
+import reactor.test.StepVerifier;
+
 @Testcontainers
 @SpringBootTest(properties = {
         "cloud.aws.stack.auto=false",
@@ -29,12 +33,14 @@ import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
         "spring.cloud.stream.bindings.input-in-0.destination=queue1",
         "spring.cloud.stream.bindings.function.definition=input"
 })
-class SqsBinderAT {
+class SqsBinderIT {
 
     @Container
-    static private LocalStackContainer localStack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.10"))
+    private static final LocalStackContainer localStack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.10"))
             .withServices(SQS)
             .withEnv("DEFAULT_REGION", "eu-central-1");
+
+    private static final Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
 
     @Autowired
     private AmazonSQSAsync amazonSQS;
@@ -48,11 +54,23 @@ class SqsBinderAT {
     }
 
     @Test
-    void canTestHealth() throws Exception {
+    void shouldPassMessageToConsumer() {
+        String testMessage = "test message";
 
+        String queueUrl = amazonSQS.getQueueUrl("queue1").getQueueUrl();
+        amazonSQS.sendMessage(queueUrl, testMessage);
+
+        StepVerifier.create(sink.asFlux())
+                .assertNext(message -> {
+                    assertThat(message).isEqualTo(testMessage);
+                })
+                .verifyTimeout(Duration.ofSeconds(1));
+    }
+
+    @Test
+    void canTestHealth() {
         assertThat(healthEndpoint.health().getStatus()).isEqualTo(Status.UP);
         assertThat(healthEndpoint.healthForPath("sqsBinder").getStatus()).isEqualTo(Status.UP);
-
     }
 
     @TestConfiguration
@@ -68,7 +86,7 @@ class SqsBinderAT {
 
         @Bean
         Consumer<String> input() {
-            return (String s) -> System.out.println(s);
+            return sink::tryEmitNext;
         }
     }
 
