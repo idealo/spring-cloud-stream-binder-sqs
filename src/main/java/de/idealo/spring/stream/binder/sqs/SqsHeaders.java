66d633293e0a04687b3c2dc405de102f5b4564fd
sixcorners <sixcorners@gmail.com>
2022-02-03 06:34:16 -0600

    feat: support sending delayed messages

--- src/main/java/de/idealo/spring/stream/binder/sqs/SqsHeaders.java
+++ src/main/java/de/idealo/spring/stream/binder/sqs/SqsHeaders.java
@@ -4,6 +4,8 @@ public final class SqsHeaders {
 
     public static final String PREFIX = "sqs_";
 
+    public static final String DELAY = PREFIX + "delay";
+
     public static final String GROUP_ID = PREFIX + "groupId";
 
     public static final String DEDUPLICATION_ID = PREFIX + "deduplicationId";
